3692449f36544b3b9e64834c613ea94c242df7ed
Cengizhan <cengizhan_894@hotmail.com>
2023-08-03 10:39:13 +0300

    feat(add strong ball state)

--- Brick-Buster-Pro/Assets/Script/State/SelectionState.cs
+++ Brick-Buster-Pro/Assets/Script/State/SelectionState.cs
@@ -20,8 +20,12 @@ public class SelectionState : BaseState
         GetCard();
         gameControlSM.threeBallBtn.onClick.AddListener(ChangeStateToTreeBall);
         gameControlSM.extensionPlatformBtn.onClick.AddListener(ChangeStateToExtensionPlatformState);
-        gameControlSM.PlatformGunBtn.onClick.AddListener(ChangeStateToPlatformGunState);
-        //gameControlSM.startButton.onClick.AddListener(changeStateToInGame);
+        gameControlSM.platformGunBtn.onClick.AddListener(ChangeStateToPlatformGunState);
+        gameControlSM.bigBallBtn.onClick.AddListener(ChangeStateToBiggBallState);
+        gameControlSM.smallBallBtn.onClick.AddListener(ChangeStateToSmallBallState);
+        gameControlSM.strongBallBtn.onClick.AddListener(ChangeStateToStronglBallState);
+        gameControlSM.fastStrongBallBtn.onClick.AddListener(ChangeStateToFastStronglBallState);
+        gameControlSM.activeBallBtn.onClick.AddListener(ChangeStateToActiveWallState);
     }
     public override void UpdateLogic()
     {
@@ -36,7 +40,7 @@ public class SelectionState : BaseState
 
 
     }
-  
+
     void StopBall()
     {
         for (int i = 0; i < gameControlSM.totalBallObjList.Count; i++)
@@ -47,7 +51,7 @@ public class SelectionState : BaseState
     public void GetCard()
     {
 
-       ShuffleCardList();
+        ShuffleCardList();
         for (int i = 0; i < 3; i++)
         {
             gameControlSM.cardList[i].gameObject.SetActive(true);
@@ -60,11 +64,11 @@ public class SelectionState : BaseState
     {
         int objectCount = gameControlSM.cardList.Count;
 
-       
+
         for (int i = 0; i < objectCount - 1; i++)
         {
-            
-            int randomIndex =UnityEngine.Random.Range(i, objectCount);
+
+            int randomIndex = UnityEngine.Random.Range(i, objectCount);
             GameObject temp = gameControlSM.cardList[randomIndex];
             gameControlSM.cardList[randomIndex] = gameControlSM.cardList[i];
             gameControlSM.cardList[i] = temp;
@@ -72,14 +76,42 @@ public class SelectionState : BaseState
     }
     void ChangeStateToTreeBall()
     {
+        gameControlSM.threeBallBtn.onClick.AddListener(ChangeStateToTreeBall);
         stateMachine.ChangeState(gameControlSM.threeBallState);
     }
     void ChangeStateToExtensionPlatformState()
     {
+        gameControlSM.extensionPlatformBtn.onClick.AddListener(ChangeStateToExtensionPlatformState);
         stateMachine.ChangeState(gameControlSM.extensionPlatformState);
     }
     void ChangeStateToPlatformGunState()
     {
+        gameControlSM.platformGunBtn.onClick.AddListener(ChangeStateToPlatformGunState);
         stateMachine.ChangeState(gameControlSM.platformGunState);
+    }
+    void ChangeStateToBiggBallState()
+    {
+        gameControlSM.bigBallBtn.onClick.AddListener(ChangeStateToBiggBallState);
+        stateMachine.ChangeState(gameControlSM.bigBallState);
+    }
+    void ChangeStateToSmallBallState()
+    {
+        gameControlSM.smallBallBtn.onClick.AddListener(ChangeStateToSmallBallState);
+        stateMachine.ChangeState(gameControlSM.smallBallState);
+    }
+    void ChangeStateToStronglBallState()
+    {
+        gameControlSM.strongBallBtn.onClick.AddListener(ChangeStateToStronglBallState);
+        stateMachine.ChangeState(gameControlSM.strongBallState);
+    }
+    void ChangeStateToFastStronglBallState()
+    {
+        gameControlSM.fastStrongBallBtn.onClick.AddListener(ChangeStateToFastStronglBallState);
+        stateMachine.ChangeState(gameControlSM.fastAndStrongBallState);
+    }
+    void ChangeStateToActiveWallState()
+    {
+        gameControlSM.activeBallBtn.onClick.AddListener(ChangeStateToActiveWallState);
+        stateMachine.ChangeState(gameControlSM.activeWallState);
     }
 }
