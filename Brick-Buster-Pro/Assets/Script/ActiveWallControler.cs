33c29f0fc98b86a2746668fa8d957bc796dad1f2
Cengizhan <cengizhan_894@hotmail.com>
2023-08-03 10:36:31 +0300

    feat(add AbilityState)
    #add activeballstate

---
+++ Brick-Buster-Pro/Assets/Script/ActiveWallControler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ActiveWallControler : MonoBehaviour
+{
+    [SerializeField] float activeTime;
+    public void StartActiveWall()
+    {
+        StartCoroutine(ActiveWallTimer());
+    }
+    IEnumerator ActiveWallTimer()
+    {
+        gameObject.SetActive(true);
+        gameObject.transform.DOScaleX(9.5f, .2f).SetEase(Ease.InElastic);
+        yield return new WaitForSeconds(activeTime);
+        gameObject.transform.DOScaleX(.5f, .2f).SetEase(Ease.InElastic).OnComplete(() => gameObject.SetActive(false));
+      
+    }
+
+}
